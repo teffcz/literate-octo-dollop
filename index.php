c63b6663d24ac6035751990592db820d941f211b
teffcz <teffenator@gmail.com>
2021-08-11 17:58:38 +0200

    Update index.php

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
 
         <?php
   $x = 0.0001;
-  for ($i = 0; $i <= 100000000; $i++) {
+  for ($i = 0; $i <= 20000000; $i++) {
     $x += sqrt($x);
   }
-  echo "OK!";
+  echo "cekani OK!";
 ?>
 
 </body>

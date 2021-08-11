dcd409577a9d1d0b8464d11b71b569d1fc51d646
teffcz <teffenator@gmail.com>
2021-08-11 15:58:59 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+        $title=gethostname();
+?>
+
+<DOCTYPE html>
+<html>
+<head>
+        <title>mkt.cz</title>
+</head>
+
+<body bgcolor="yellow">
+
+        <table style='margin: auto; width: 1400px; border: solid 1px black; text-align: center;'>
+         <tr> <th style='text-decoration: underline;'><?php echo $title; ?></th> </tr>
+
+<?php
+        $radku = 20;
+        $soubor = file("access.log");
+        // vystup radku - jde nam pouze o poslednich $radku
+        for ($i = max(0, count($soubor)-$radku); $i < count ($soubor); $i++) {
+                echo "<tr> <td>".$soubor[$i]."</td> \n";
+        }
+?>
+
+        </table>
+
+</body>
+</html>

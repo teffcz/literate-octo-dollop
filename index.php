5c40225a625e572dae5024c9910810394acdb854
teffcz <teffenator@gmail.com>
2021-11-09 09:31:41 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,36 +1,14 @@
 <?php
         $title=gethostname();
 ?>
-
 <DOCTYPE html>
 <html>
 <head>
         <title><?php $title ?></title>
 </head>
-
-<body bgcolor="yellow">
-
+<body bgcolor="white">
         <table style='margin: auto; width: 1400px; border: solid 1px black; text-align: center;'>
          <tr> <th style='text-decoration: underline;'><?php echo $title; ?></th> </tr>
-
-         <?php /*
-        $radku = 20;
-        $soubor = file("access.log");
-        // vystup radku - jde nam pouze o poslednich $radku
-        for ($i = max(0, count($soubor)-$radku); $i < count ($soubor); $i++) {
-                echo "<tr> <td>".$soubor[$i]."</td> \n";
-        }*/
-?>
-
         </table>
-
-        <?php
-  $x = 0.0001;
-  for ($i = 0; $i <= 20000000; $i++) {
-    $x += sqrt($x);
-  }
-  echo "cekani OK!";
-?>
-
 </body>
 </html>
